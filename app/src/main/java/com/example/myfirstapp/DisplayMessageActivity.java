f977d484fbf768e794870b84344a334e1a16c65d
josiahhopkins <hopkinsjosiah@gmail.com>
2017-04-05 15:39:53 -0700

    Add second activity functionality

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE));
     }
 }
